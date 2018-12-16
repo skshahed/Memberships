6ac3d2a7def2df4407bde1d04bc415ed6b588a51
skshahed <sha_hed007@yahoo.com>
2018-12-17 00:26:01 +0600

    Initial Commit

--- Memberships/App_Start/BundleConfig.cs
+++ Memberships/App_Start/BundleConfig.cs
@@ -22,10 +22,14 @@ namespace Memberships
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/admin").Include(
+                      "~/Scripts/AdminMenu.js"));
+
         }
     }
 }
